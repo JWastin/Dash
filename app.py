debefdc3fbf84bfc4bfbeadb409482beb0451bfa
MoonPi <109249912+JWastin@users.noreply.github.com>
2023-04-13 15:14:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-from dash import Dash
+from dash import Dash, dcc, html
 
 
 app = Dash(__name__)
